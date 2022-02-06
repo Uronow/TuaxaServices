2c568d0b459b537b9a4deb222d4a49cc67560414
Uronow <83173867+Uronow@users.noreply.github.com>
2022-02-06 20:07:17 +0300

    Update main.lua

--- status/main.lua
+++ status/main.lua
@@ -1,3 +1,4 @@
-local ststbl = {[1] = {name="Tuaxascript.com",status="1",note="Operational"},[2] = {"Tuaxa Scripts",status="1",note="Operational"},[3] = {"Tuaxa Cloud",status="1",note="Operational"},[4] = {"Zort Technologies",status="1",note="Operational"},[5] = {"Other Codes",status="1",note="Operational"}}
+--V1
+local ststbl = {[1] = {name="Tuaxascript.com",status="1",note="Operational"},[2] = {name="Tuaxa Scripts",status="1",note="Operational"},[3] = {name="Tuaxa Cloud",status="1",note="Operational"},[4] = {name="Zort Technologies",status="1",note="Operational"},[5] = {name="Other Codes",status="1",note="Operational"}}
 
 return ststbl
